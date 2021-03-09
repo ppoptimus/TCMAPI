821dba801792cfc14847ff0ed9616eae2a6354f8
dev01tcm <78786837+dev01tcm@users.noreply.github.com>
2021-03-09 10:34:54 +0700

    add homepage

---
+++ TCMAPI/Controllers/DeleteMenuController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using TCMAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TCMAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeleteMenuController : ControllerBase
+    {
+        private readonly IOptions<AppSettingModel> appSettings;
+        public DeleteMenuController(IOptions<AppSettingModel> app)
+        {
+            appSettings = app;
+        }
+
+        // POST api/<DeleteMenuController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+    }
+}
--- TCMAPI/Controllers/GetRichMenuListController.cs
+++ TCMAPI/Controllers/GetRichMenuListController.cs
@@ -22,17 +22,26 @@ namespace TCMAPI.Controllers
         {
             string str = "";
 
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(appSettings.Value.GetRichMenuListUrl);
-            string lineAccessToken = appSettings.Value.LineChannelAccessToken;
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Method = "GET";
-            httpWebRequest.Headers.Add("Authorization", lineAccessToken);
-            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            try
             {
-                str = streamReader.ReadToEnd();
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(appSettings.Value.GetRichMenuListUrl);
+                string lineAccessToken = appSettings.Value.LineChannelAccessToken;
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "GET";
+                httpWebRequest.Headers.Add("Authorization", lineAccessToken);
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    str = streamReader.ReadToEnd();
+                }
             }
+            catch (System.Exception ex)
+            {
+                str = ex.Message;
+                return NotFound(str);
+            }
+            
 
             return Ok(str);
         }
